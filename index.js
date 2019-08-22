6df4dd0e9b4aa402a9e4e77ed3acfd07c0aea9c7
koreeda <koreeda@groovy-m.com>
2019-08-22 15:54:34 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.helloPubSub = (event, context) => {
+    const pubsubMessage = event.data;
+    console.log(Buffer.from(pubsubMessage, 'base64').toString());
+
+    console.info('helloPubSub finish.');
+};
